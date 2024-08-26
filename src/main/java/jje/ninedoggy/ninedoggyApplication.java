be3fba7860bf5a10735e283a41849eb193f672a2
applebloom1214 <rainbow610@naver.com>
2024-08-26 20:57:42 +0900

    created project

---
+++ src/main/java/jje/ninedoggy/ninedoggyApplication.java
@@ -0,0 +1,11 @@
+package jje.ninedoggy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ninedoggyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ninedoggyApplication.class, args);
+    }
+}
