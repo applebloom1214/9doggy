1964360202b4f1d0ffcf73d1fcdc94b92d6cf5d2
applebloom1214 <rainbow610@naver.com>
2024-08-29 20:44:34 +0900

    make post entity

--- src/main/java/jje/ninedoggy/domain/Post.java
+++ src/main/java/jje/ninedoggy/domain/Post.java
@@ -1,4 +1,51 @@
 package jje.ninedoggy.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bno", updatable = false)
+    private Long bno;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "writer", nullable = false)
+    private String writer;
+
+    @CreatedDate
+    private LocalDateTime date;
+
+    @Column(name = "hit", nullable = false)
+    @ColumnDefault("0")
+    private Long hit;
+
+    @Column(name = "like", nullable = false)
+    @ColumnDefault("0")
+    private Long like;
+
+    @Builder
+    public Post(String title, String content, String writer) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+    }
+
+
 }
---
+++ src/main/java/jje/ninedoggy/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package jje.ninedoggy.repository;
+
+import jje.ninedoggy.domain.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Post, Long> {
+}
