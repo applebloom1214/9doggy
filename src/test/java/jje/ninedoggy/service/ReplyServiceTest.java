759036b3da082a975dcb97aeb8749fcec850d04f
applebloom1214 <rainbow610@naver.com>
2024-12-16 20:45:11 +0900

    aws s3 setting 2,

--- src/test/java/jje/ninedoggy/service/ReplyServiceTest.java
+++ src/test/java/jje/ninedoggy/service/ReplyServiceTest.java
@@ -21,7 +21,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ReplyServiceTest {
