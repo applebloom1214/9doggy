1c2ab119c4ec284836d399fa0f2276c800ba05f2
applebloom1214 <rainbow610@naver.com>
2025-04-14 19:30:39 +0900

    push error

--- src/main/resources/static/js/write.js
+++ src/main/resources/static/js/write.js
@@ -33,5 +33,5 @@ while(upload__files.hasChildNodes()) {
 })
 
 function loadThumbImg(files) {
-    abcdefghijklmnopqrstuvwxyz0123456789012345678901234
+    abcdefghijklmnopqrstuvwxyz01234567890123456789012345
 }
